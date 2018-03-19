33b61e1a64333791c009c87db0ca050c4e3522f3
Alexandra Ralev <alexandrapredaralev@gmail.com>
2018-03-19 18:21:40 +0000

    clean up and comment js

--- js/app.js
+++ js/app.js
@@ -1,24 +1,13 @@
 const icons = ['ambulance', 'anchor', 'balance-scale', 'basketball-ball', 'bath', 'bed', 'beer', 'bicycle', 'binoculars', 'bomb', 'bug', 'car', 'chess-rook', 'chess-queen', 'cloud', 'fighter-jet', 'fire', 'gamepad', 'home', 'sun', 'volleyball-ball', 'chess-knight'];
 const board = document.querySelector('.game-board');
 const reset = document.getElementById('reset');
+const form = document.getElementById('form');
 const difficulties = document.querySelectorAll("input[name='difficulty']");
 let clickCount = 0;
-let clickedCards = [];
-let sec = 0;
+let clickedCards;
+let sec;
 let difficulty;
 let difficultyClass;
-
-function startGame() {
-	
-	checkDifficulty();
-	
-	//start game in normal mode
-	populate(difficulty);
-
-	//timerReset();
-	sec = 0;
-	clickedCards = [];
-}
 
 
 //shuffle function from https://bost.ocks.org/mike/shuffle/
@@ -32,88 +21,7 @@ function shuffle(array) {
   }
 }
 
-function populate(num) {
-
-	//clear the board
-	board.innerHTML = "";
-
-	//LOGIC IS: shuffle the main array and slice half the number of cards
-	shuffle(icons);
-	let boardIcons = icons.slice(0, num/2);
-	
-	//duplicate the array values to make pairs and shuflfe this new array
-	boardIcons = boardIcons.concat(boardIcons);
-	shuffle(boardIcons);
-	
-	//populate HTML
-	const fragment = document.createDocumentFragment(); 
-
-	for (let x = 0; x < num; x++) {
-    	const card = document.createElement('div');
-    	const icon = document.createElement('i')
-    	icon.classList.add('card', 'fas', 'fa-' + boardIcons[x]);
-    	icon.classList.add(difficultyClass);
-		card.appendChild(icon);
-		fragment.appendChild(card);
-		console.log('it works');
-	}
-	board.appendChild(fragment); 
-}
-
-board.addEventListener('click', e => {
-	
-	if (e.target.classList.contains('fas') && !e.target.classList.contains('open')) {
-		clickCount += 1;
-		console.log(clickCount);
-		clickedCards.push(e.target.classList[2]);
-		console.log(clickedCards);
-		e.target.classList.add('open');
-		if (clickCount === 2) {
-			clickCount = 0;
-			let selectedCards = document.querySelectorAll('.open');
-			if (clickedCards[0]===clickedCards[1]) {
-					console.log('match');
-					clickedCards = [];
-					[].forEach.call(selectedCards, c =>{
-						c.classList.add('correct');
-					});
-
-			} else {
-					console.log('no match');
-					//set timeout here so they can see the cards before they flip
-					setTimeout(function(){
-						clickedCards = [];
-						[].forEach.call(selectedCards, c =>{
-						c.classList.remove('open');
-						});
-					}, 1200);
-					
-			}
-		}				
-	}
-});
-
-setInterval(timer, 1000);
-function timer(){
-	const timer = document.getElementById('timer');
-	sec+=1;
-	if (sec<60) {
-		timer.innerText = sec;
-	} else if (sec<3600) {
-		let minutes = Math.floor(sec/60);
-		let seconds = sec % 60;
-		timer.innerText = minutes+":"+seconds;
-	}
-}
-
-reset.addEventListener('click', startGame);
-
-
-//check difficulty radio
-
-
-
-
+// go over the radio buttons and check the difficulty selection
 function checkDifficulty(){
 	[].forEach.call(difficulties, function(val){
 		
@@ -130,13 +38,100 @@ function checkDifficulty(){
 			difficultyClass = 'hard';
 		}
 	});
-
 }
 
-document.getElementById('form').addEventListener('change', function(){
+//populate board with cards
+function populate(num) {
+
+	//clear previous clicks and memorised cards
+	clickedCards = [];
+	clickCount = 0;
+
+	//clear the board
+	board.innerHTML = "";
+
+	//LOGIC IS: shuffle the main array and slice half the number of cards
+	//this is to always get a random selection of icons
+	shuffle(icons);
+	let boardIcons = icons.slice(0, num/2);
+	
+	//duplicate the array values to make pairs and shuflfe this new array
+	boardIcons = boardIcons.concat(boardIcons);
+	shuffle(boardIcons);
+	
+	//actually populate HTML
+	const fragment = document.createDocumentFragment(); 
+
+	for (let x = 0; x < num; x++) {
+    	const card = document.createElement('div');
+    	const icon = document.createElement('i')
+    	icon.classList.add('card', 'fas', 'fa-' + boardIcons[x]);
+    	icon.classList.add(difficultyClass);
+		
+		card.appendChild(icon);
+		fragment.appendChild(card);
+	}
+	board.appendChild(fragment); 
+}
+
+//timer functionality
+setInterval(timer, 1000);
+function timer(){
+	const timer = document.getElementById('timer');
+	sec+=1;
+	if (sec<60) {
+		timer.innerText = sec;
+	} else if (sec<3600) {
+		let minutes = Math.floor(sec/60);
+		let seconds = sec % 60;
+		timer.innerText = minutes+":"+seconds;
+	}
+}
+
+function startGame() {
 	checkDifficulty();
 	populate(difficulty);
-});
+	sec = 0;  //timer reset
+}
 
+function matchChecker(e){
+	//LOGIC IS: make sure the click target is a card, it is not currently open for checking and it is not permanenetly checked as correct 
+	if (e.target.classList.contains('fas') && !e.target.classList.contains('open') && !e.target.classList.contains('correct')){
+		clickCount += 1;
+
+		// make an array of the clicked cards' font Awesome classes		
+		clickedCards.push(e.target.classList[2]);
+
+		e.target.classList.add('open');
+
+		if (clickCount === 2) {
+			clickCount = 0;
+			let selectedCards = document.querySelectorAll('.open');
+			//check if the clicked cards' font Awesome classes match
+			if (clickedCards[0]===clickedCards[1]) {
+				clickedCards = [];
+				//if it's a match, add the class 'correct' to keep the cards open
+				[].forEach.call(selectedCards, c =>{
+					c.classList.add('correct');
+				});
+
+			} else {
+				//if it's not a match, remove the class 'open' so the cards close again
+				//set timeout here so the player can see the cards before they flip(must refactor this 'cause weird errors)
+				setTimeout(function(){
+					clickedCards = [];
+					[].forEach.call(selectedCards, c =>{
+					c.classList.remove('open');
+					});
+				}, 1200);
+					
+			}
+		}				
+	}
+}
+
+reset.addEventListener('click', startGame);
+form.addEventListener('change', startGame);
+board.addEventListener('click', matchChecker);
 
 startGame();
